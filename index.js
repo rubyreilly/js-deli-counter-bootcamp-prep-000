091707f3e4f7a505f0f9b7521311876359c2279b
Ruby Reilly <rubyclaroreilly@gmail.com>
2018-07-23 17:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var katzDeliLine= [];
 
 function takeANumber(katzDeliLine,name){
-  if (katzDeliLine.length===0){
-    katzDeliLine.push(name);
-    return (`Welcome, ${name}. You are number 1 in line.`);
-  }
-  else{
-    katzDeliLine.push(name);
-    return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
-  }
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
 function nowServing(katzDeliLine){
